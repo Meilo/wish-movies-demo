08a6bb57cd0c2ea93be683c7aa54e0e3520f74e4
Ludovic Elice <ludovic.elice@gmail.com>
2021-10-17 22:23:44 +0200

    💡 Add todo comments

--- src/view/api/repositories.ts
+++ src/view/api/repositories.ts
@@ -6,8 +6,8 @@ import getMyWishlist from "view/api/getMyWishlist";
 export const wishlistRepository = {
   getMyWishlist,
   getItemStatusInWishlist,
-  addMovieInWishlist: () => Promise.resolve({ statusCode: 200 }),
-  removeMovieInWishlist: () => Promise.resolve({ statusCode: 200 }),
+  addMovieInWishlist: () => Promise.resolve({ statusCode: 200 }), // Todo: addMovieInWishlist
+  removeMovieInWishlist: () => Promise.resolve({ statusCode: 200 }), // Todo: removeMovieInWishlist
 };
 
 export const moviesRepository = {
